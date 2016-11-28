e3d3c89a40fffcbafd26c8397fd7bc67f32cc6bd
真人 <weizhen.wang@duitang.com>
2016-11-28 18:27:27 +0800

    feat: add socket.io to support socket.io 0.*

--- src/main/java/com/suphawking/collector/core/socketio/huobitest.java
+++ src/main/java/com/suphawking/collector/core/socketio/huobitest.java
@@ -1,8 +1,5 @@
 package com.suphawking.collector.core.socketio;
 
-import io.socket.client.IO;
-import io.socket.client.Socket;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -12,17 +9,37 @@ import lombok.extern.slf4j.Slf4j;
 public class huobitest {
   public static void main(String[] args0) throws Exception {
     IO.Options opts = new IO.Options();
-    opts.reconnection = true;
-    final Socket socket = IO.socket("http://hq.huobi.com:80/",opts);
-    String strMsg = "{\"symbolList\":{\"lastTimeLine\":[{\"symbolId\":\"btccny\",\"pushType\":\"pushLong\"}],"
-        +                  "},\"version\":1,\"msgType\":\"reqMsgSubscribe\",\"requestIndex\":1404103038520}";
+    //opts.reconnection = true;
+
+    //final Socket socket = IO.socket("hq.huobi.com:80",opts);
+    //String strMsg = "{\"symbolList\":{\"lastTimeLine\":[{\"symbolId\":\"btccny\",\"pushType\":\"pushLong\"}],"
+    //   +                  "},\"version\":1,\"msgType\":\"reqMsgSubscribe\",\"requestIndex\":1404103038520}";
 
 
-    socket.on(Socket.EVENT_CONNECT, args -> {
-      log.info("Connected.");
+    //socket.on(Socket.EVENT_CONNECT,new Emitter.Listener() {
+    //  @Override
+    //  public void call(Object... args) {
+    //    log.info("Connected.");
+    //    //JSONObject obj = (JSONObject)args[0];
+    //  }});
 
-      socket.emit("requests", strMsg);
-    }).on(Socket.EVENT_DISCONNECT, args -> log.info("Disconnected."));
-    socket.connect();
+
+    //socket.on("message", args -> log.info("message: {}", args[0]));
+    //socket.connect();
   }
 }
+//@Slf4j
+//class JsonLogger implements Emitter.Listener {
+//
+//  private String prefix;
+//
+// public JsonLogger(String prefix) {
+//   this.prefix = prefix;
+//  }
+
+//  @Override
+//  public void call(Object... args) {
+//    JSONObject json = (JSONObject) args[0];
+//    log.info("{}: {}",prefix, json);
+//  }
+//}
