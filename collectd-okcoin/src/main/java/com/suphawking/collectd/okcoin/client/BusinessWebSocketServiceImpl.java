57e5a20b263a3cba06b9c5d40d9962f525a85d6b
真人 <weizhen.wang@duitang.com>
2017-01-07 22:22:19 +0800

    feat: okcoin trades data

--- collectd-okcoin/src/main/java/com/suphawking/collectd/okcoin/client/BusinessWebSocketServiceImpl.java
+++ collectd-okcoin/src/main/java/com/suphawking/collectd/okcoin/client/BusinessWebSocketServiceImpl.java
@@ -2,8 +2,8 @@ package com.suphawking.collectd.okcoin.client;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.suphawking.collectd.spi.domain.data.huobi.SubSpotcnyBtcTicker;
 import com.suphawking.collectd.okcoin.WebSocketService;
+import com.suphawking.collectd.spi.domain.data.huobi.SubSpotcnyBtcTicker;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,8 +17,6 @@ import java.util.List;
 public class BusinessWebSocketServiceImpl implements WebSocketService {
 
 
-
-
   public String pong = "{\"event\":\"pong\"}";
 
 
@@ -29,12 +27,30 @@ public class BusinessWebSocketServiceImpl implements WebSocketService {
   public void onReceive(String msg) {
     log.debug("WebSocket Client received message: " + msg);
     if (!msg.equals(pong)) {
-      List<HashMap> data =  JSONArray.parseArray(msg, HashMap.class);
-      if (data.get(0).get("data") != null ) {
-        SubSpotcnyBtcTicker tickerData = JSON.parseObject(data.get(0).get("data").toString(),
-            SubSpotcnyBtcTicker.class);
+      List<HashMap> data = JSONArray.parseArray(msg, HashMap.class);
+      HashMap realDate = data.get(0);
+      if (realDate.get("data") != null) {
 
-        //log.info(tickerData.getBuy().toString());
+        switch (realDate.get("channel").toString()) {
+          case "ok_sub_spotcny_btc_ticker":
+            SubSpotcnyBtcTicker tickerData = JSON.parseObject(data.get(0).get("data").toString(),
+                SubSpotcnyBtcTicker.class);
+            log.info(tickerData.toString());
+            break;
+          case "ok_sub_spotcny_btc_trades":
+
+            log.info(data.get(0).get("data").toString());
+            List<List<String>> tradaDate = JSON.parseObject(data.get(0).get("data").toString(),
+                List.class);
+            //log.debug(tradaDate.get(0).toString());
+            tradaDate.parallelStream().forEach( n -> System.out.println(n));
+            break;
+          default:
+            break;
+        }
+
+
+        //
       }
     }
   }
