8e077d498aef8a0edb1058c50e576baac340e79c
真人 <weizhen.wang@duitang.com>
2017-01-07 14:57:49 +0800

    okcoin trend data add to jdbi

--- collectd-main/src/main/java/com/suphawking/collectd/jdbi/JdbiModule.java
+++ collectd-main/src/main/java/com/suphawking/collectd/jdbi/JdbiModule.java
@@ -1,12 +1,16 @@
 package com.suphawking.collectd.jdbi;
 
 import com.google.common.base.Preconditions;
+import com.google.inject.Singleton;
 import com.suphawking.collectd.AppCfg;
 import com.suphawking.collectd.CollectdModule;
+import com.suphawking.collectd.okcoin.dao.SaveOkcoinTrendData;
+import com.suphawking.collectd.okcoin.repo.OkcoinTrendDataRepo;
 
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Environment;
 
+import lombok.Singular;
 import lombok.extern.slf4j.Slf4j;
 
 import org.skife.jdbi.v2.DBI;
@@ -16,6 +20,7 @@ import org.skife.jdbi.v2.DBI;
  */
 @Slf4j
 public class JdbiModule extends CollectdModule {
+  private SaveOkcoinTrendData saveOkcoinTrendData;
   public JdbiModule(AppCfg cfg, Environment env) {
     super(cfg, env);
     initJdbi();
@@ -27,12 +32,13 @@ public class JdbiModule extends CollectdModule {
     Preconditions.checkNotNull(env);
     final DBIFactory factory = new DBIFactory();
     final DBI jdbiCollector = factory.build(env, cfg.getCollectorDbFactory(), "collector-db");
+    this.saveOkcoinTrendData = jdbiCollector.onDemand(SaveOkcoinTrendData.class);
+
   }
 
   @Override
   protected void configure() {
-
+    bind(SaveOkcoinTrendData.class).toInstance(this.saveOkcoinTrendData);
+    bind(OkcoinTrendDataRepo.class).in(Singleton.class);
   }
-
-
 }
